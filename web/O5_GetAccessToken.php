394b784c2e0fe806c52f1553a054e005aa6b4707
Marcos Lazarin <hoyaspt@gmail.com>
2016-12-20 06:51:17 -0600

    refactoring checkout files

--- web/O5_GetAccessToken.php
+++ web/O5_GetAccessToken.php
@@ -1,5 +1,5 @@
 <?php
-require_once (dirname(__DIR__)) . '/src/controller/MasterPassController.php';
+require_once (dirname(__DIR__)) . '/src/checkout/controller/MasterPassController.php';
 
 session_start();
 $sad = unserialize($_SESSION['sad']);
